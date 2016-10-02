b2aea7d36ba5ccef30473b0ab395c90fbf830beb
Skemp45 <Shawn@besmartdesigns.com>
2016-10-01 23:06:02 -0700

    first commit

---
+++ better-search.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Better Search
+ * Description: This plugin will leverage a better search functionality for help articles that pertain to your location in the admin panel.
+ * Version: 1.0
+ * Author: Be Smart Designs
+ * Author URI: http://besmartdesigns.com
+ * License: GPL2
+ */
+
+/*  Copyright 2016  Be Smart Designs  (email : shawn@besmartdesigns.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+**************************************************************************/
